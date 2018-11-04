87b3c2802ebc006c6a0e38d3224975e1acfaf7b5
holdercp <chrispholder@gmail.com>
2018-11-04 18:02:18 -0500

    Convert SearchForm to controlled component

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchForm = () => (
-  <form className="search-form">
-    <label htmlFor="search">Search</label>
-    <input type="text" name="search" id="search" placeholder="Ninjas" />
-    <button type="submit">Search</button>
-  </form>
-);
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  render() {
+    const { value } = this.state;
+    return (
+      <form className="search-form">
+        <label htmlFor="search">Search</label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Ninjas"
+          value={value}
+          onChange={this.handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+    );
+  }
+}
 
 export default SearchForm;
