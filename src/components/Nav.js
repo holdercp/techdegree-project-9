5f1b4f584e9422a52b5420804b416ff55a352faa
holdercp <chrispholder@gmail.com>
2018-11-18 15:44:27 -0500

    Change Links to NavLinks for styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Nav = ({ linkNames }) => {
   const links = linkNames.map(name => (
     <li key={name}>
-      <Link to={`/${name.toLowerCase()}`}>{name}</Link>
+      <NavLink to={`/${name.toLowerCase()}`} activeClassName="active">
+        {name}
+      </NavLink>
     </li>
   ));
 
